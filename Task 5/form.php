401af38df33a492c8d61ae5a91d89451ca8feba3
Anton Mezentsev <160131167+qcrymoon@users.noreply.github.com>
2024-05-27 18:16:56 +0300

    hhh

---
+++ Task 5/form.php
@@ -0,0 +1,7 @@
+
+<!-- Форма для завантаження файлу -->
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="text" name="fileName" placeholder="Введіть назву файлу">
+    <input type="submit" name="submit" value="Завантажити файл">
+</form>
